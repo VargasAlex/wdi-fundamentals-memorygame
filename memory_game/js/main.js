4f0949da21ee24bb5233520c11c80d69b0aed58e
Alex Vargas <alexv2489@gmail.com>
2018-05-07 21:08:09 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,24 @@
-console.log("Up and running!");
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
-var cardOne = ["queen"];
-var cardTwo = ["king"];
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardTwo")
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
-} if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else {
-	alert("Sorry, try again");
+	alert("Sorry, try again");}
+};
+
+var flipCard = function (cardId) {
+	console.log ("User flipped " + cards[cardId]);
+	if (cardsInPlay.length === 2){
+		checkForMatch();
 }
-console.log();
+};
+flipCard(0);
+flipCard(2);
+cardsInPlay.push("cards[cardId]");
+
+
+
 
 
 
